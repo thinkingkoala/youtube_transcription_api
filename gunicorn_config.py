1afc1afd27d6814367f44373b4847005905518ea
thinkingkoala <privalikhinanadia@gmail.com>
2024-08-06 10:01:10 +0200

    fix

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,8 +4,11 @@ def pre_request(worker, req):
     
     # Check and set Content-Length if not present
     if 'content-length' not in headers:
-        req_body = req.get_data(as_text=False)  # Get the raw request body
-        content_length = str(len(req_body))
+        body_length = 0
+        if hasattr(req, 'body'):
+            body_length = len(req.body)
+        
+        content_length = str(body_length)
         req.headers.append(('Content-Length', content_length))
     
     # Check and set Host if not present
