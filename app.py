a50dda3fcad1b41381615cd13533899ce9b8f86d
thinkingkoala <privalikhinanadia@gmail.com>
2024-08-07 14:31:40 +0200

    proxy update

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def get_youtube_id(url):
 
 def process_transcript(video_id):
     proxy_address=os.environ.get("PROXY")
-    transcript = YouTubeTranscriptApi.get_transcript(video_id, proxies = {"https": proxy_address})
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, proxies = {"http": proxy_address,"https": proxy_address})
     full_text = ' '.join([entry['text'] for entry in transcript])
     return full_text
 
